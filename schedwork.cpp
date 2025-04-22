dd7a887526630f90f7614d28676acff073e3a4f6
Aiden Howard <achoward@usc.edu>
2025-04-22 22:27:45 +0000

    Final Commit

--- schedwork.cpp
+++ schedwork.cpp
@@ -22,6 +22,41 @@ static const Worker_T INVALID_ID = (unsigned int)-1;
 
 // Add prototypes for any helper functions here
 
+bool schedHelp(
+    const AvailabilityMatrix& avail,
+    size_t dailyNeed, 
+    size_t maxShifts, 
+    DailySchedule& sched, 
+    vector<size_t>& shifts, 
+    size_t day,
+    size_t slot
+){
+    if(day == avail.size()){
+        return true;
+    }
+    if(slot == dailyNeed){
+        return schedHelp(avail, dailyNeed, maxShifts, sched, shifts, day + 1, 0);
+    }
+
+    for(Worker_T w = 0; w < avail[0].size(); ++w){
+        if(avail[day][w] && shifts[w] < maxShifts && 
+        find(sched[day].begin(), sched[day].end(), w) == sched[day].end()){
+            sched[day].push_back(w);
+            shifts[w]++;
+
+
+            if(schedHelp(avail, dailyNeed, maxShifts, sched, shifts, day, slot + 1)){
+                return true;
+            }
+
+            sched[day].pop_back();
+            shifts[w]--;
+        
+        }
+    }
+    return false;
+}
+
 
 // Add your implementation of schedule() and other helper functions here
 
@@ -38,7 +73,12 @@ bool schedule(
     sched.clear();
     // Add your code below
 
+    //add prints to trace / debug
+    //keep in mind where you put it, think of it as going down levels of the tree
 
+    sched.assign(avail.size(), vector<Worker_T>());
+    vector<size_t> shiftsWorked(avail[0].size(), 0);
+    return schedHelp(avail, dailyNeed, maxShifts, sched, shiftsWorked, 0, 0);
 
 
 }
--- wordle.cpp
+++ wordle.cpp
@@ -14,7 +14,54 @@ using namespace std;
 
 // Add prototypes of helper functions here
 
+void func(
+  string current,
+  string floating, 
+  const set<string>& dict, 
+  set<string>& results,
+  size_t index){
 
+//base case
+    if(index == current.length()){
+      //floating letters test
+      if(floating.empty() && dict.find(current) != dict.end()){
+        results.insert(current);
+       
+      }
+
+      return;
+    }
+  
+    if(current[index] != '-'){ // for used letters
+      func(current, floating, dict, results, index + 1);
+        return;
+    }
+
+    for(char c = 'a'; c <= 'z'; ++c){ //start at first lowercase, loop til end
+      current[index] = c;
+
+      size_t pos = floating.find(c);
+      if(pos < floating.size()){
+        string newFloat = floating;
+        newFloat.erase(pos,1);
+        func(current, newFloat, dict, results, index + 1);
+      }
+      else{
+        int blanksRemain = 0;
+        for(size_t j = index + 1; j < current.size(); ++j){
+          if(current[j] == '-'){
+            ++blanksRemain;
+          }
+        }
+        if(floating.size() <= blanksRemain){
+          func(current, floating, dict, results, index + 1);
+        }
+      }
+    }
+    
+
+    current[index] = '-';
+  }
 // Definition of primary wordle function
 std::set<std::string> wordle(
     const std::string& in,
@@ -23,6 +70,9 @@ std::set<std::string> wordle(
 {
     // Add your code here
 
+  set<string> results;
+  func(in, floating, dict, results, 0);
+  return results;
 }
 
 // Define any helper functions here
